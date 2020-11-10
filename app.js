7c7fe09d5a59ec97d2d77a0d447d5cc3e789e0a7
bofletcher <hfletche.hf@gmail.com>
2020-11-10 11:53:01 -0500

    working tip calculator.

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ submitBtn.addEventListener("click", calculateBill)
 
 function calculateBill() {
   let billAmt = document.getElementById('billAmt').value;
+    if(billAmt == null || billAmt == ""){
+      alert("Bill amount cannot be blank")
+      return false
+    }
+  
   let serviceMultiplier = document.getElementById('serviceMult').value;
+
   let numberOfPeopleSharing = document.getElementById('numOfPeople').value;
+    if(numberOfPeopleSharing == null || numberOfPeopleSharing == "") {
+      alert("Number of people cannot be blank")
+      return false
+    }
+
   let calculatedAmt = (billAmt * serviceMultiplier) / numberOfPeopleSharing;
-  document.getElementById('calculatedAmt').innerHTML = calculatedAmt;
+    document.getElementById('calculatedAmt').innerHTML = calculatedAmt;
+
   return calculatedAmt;
 }
